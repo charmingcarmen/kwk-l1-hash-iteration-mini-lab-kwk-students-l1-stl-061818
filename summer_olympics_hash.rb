d5f5966b560213a87c5fdef2e70505428fd5237e
charmingcarmen <bradleyc755@ritenourschools.org>
2018-06-20 19:35:14 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,9 +1,9 @@
 
 def create_olympics_hash
- "sydney"=>"2000",
+summer_olmpics=>{ "sydney"=>"2000",
 "athens"=>"2004",
 "beijing"=>"2008",
-"london"=>"2012",
+"london"=>"2012",}
 end
 
 def add_a_vaule_pair
